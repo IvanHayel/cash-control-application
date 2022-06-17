fa29dc600e4bfd5e042a8bdc56af57f5b2289197
Hayel <ivanforwork@yandex.ru>
2022-06-18 00:54:51 +0300

    Authentication entry point added.

---
+++ backend/cash-control-server/src/main/java/by/hayel/cash/control/server/security/jwt/AuthenticationEntryPointJwt.java
@@ -0,0 +1,44 @@
+package by.hayel.cash.control.server.security.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@Slf4j
+public class AuthenticationEntryPointJwt implements AuthenticationEntryPoint {
+  private static final String STATUS_FIELD = "status";
+  private static final String ERROR_FIELD = "error";
+  private static final String MESSAGE_FIELD = "message";
+  private static final String PATH_FIELD = "path";
+
+  private static final String UNAUTHORIZED_LOG = "Unauthorized error -> {}";
+  private static final String UNAUTHORIZED = "Unauthorized";
+
+  @Override
+  public void commence(
+      HttpServletRequest request,
+      HttpServletResponse response,
+      AuthenticationException authException)
+      throws IOException {
+    log.error(UNAUTHORIZED_LOG, authException.getMessage());
+    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    final Map<String, Object> body = new HashMap<>();
+    body.put(ERROR_FIELD, UNAUTHORIZED);
+    body.put(PATH_FIELD, request.getServletPath());
+    body.put(MESSAGE_FIELD, authException.getMessage());
+    body.put(STATUS_FIELD, HttpServletResponse.SC_UNAUTHORIZED);
+    final ObjectMapper mapper = new ObjectMapper();
+    mapper.writeValue(response.getOutputStream(), body);
+  }
+}
