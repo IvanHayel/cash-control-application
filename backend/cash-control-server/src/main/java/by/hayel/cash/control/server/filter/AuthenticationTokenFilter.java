4ffa982b767b26c23577dba0dc3da5471027744f
Hayel <ivanforwork@yandex.ru>
2022-06-18 00:53:41 +0300

    Token filter added.

---
+++ backend/cash-control-server/src/main/java/by/hayel/cash/control/server/filter/AuthenticationTokenFilter.java
@@ -0,0 +1,56 @@
+package by.hayel.cash.control.server.filter;
+
+import by.hayel.cash.control.server.service.JwtService;
+import lombok.AccessLevel;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class AuthenticationTokenFilter extends OncePerRequestFilter {
+  private static final String AUTHENTICATE_ERROR_LOG = "Unable to authenticate user -> {}";
+
+  JwtService jwtService;
+  UserDetailsService userDetailsService;
+
+  @Override
+  protected void doFilterInternal(
+      @NonNull HttpServletRequest request,
+      @NonNull HttpServletResponse response,
+      @NonNull FilterChain filterChain)
+      throws ServletException, IOException {
+    try {
+      String token = jwtService.getTokenFromRequest(request);
+      if (token != null && jwtService.isTokenValid(token)) {
+        String username = jwtService.getUsernameFromToken(token);
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        var authorities = userDetails.getAuthorities();
+        var authentication =
+            new UsernamePasswordAuthenticationToken(userDetails, null, authorities);
+        var authenticationDetails = new WebAuthenticationDetailsSource().buildDetails(request);
+        authentication.setDetails(authenticationDetails);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+      }
+    } catch (Exception e) {
+      log.warn(AUTHENTICATE_ERROR_LOG, e.getMessage());
+    }
+    filterChain.doFilter(request, response);
+  }
+}
