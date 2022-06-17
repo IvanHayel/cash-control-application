fa2d550c12977e5b3aea4827da8cb53420763e17
Hayel <ivanforwork@yandex.ru>
2022-06-18 00:50:44 +0300

    Main Spring Boot class added.

---
+++ backend/cash-control-server/src/main/java/by/hayel/cash/control/server/CashControlApplication.java
@@ -0,0 +1,11 @@
+package by.hayel.cash.control.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashControlApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(CashControlApplication.class, args);
+  }
+}
