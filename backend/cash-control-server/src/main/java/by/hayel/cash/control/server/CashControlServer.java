bda854c65d7681a4e2a95f88d1b0679f13ca2ceb
Hayel <ivanforwork@yandex.ru>
2022-06-18 01:04:43 +0300

    CashControlApplication -> CashControlServer

--- backend/cash-control-server/src/main/java/by/hayel/cash/control/server/CashControlServer.java
+++ backend/cash-control-server/src/main/java/by/hayel/cash/control/server/CashControlServer.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CashControlApplication {
+public class CashControlServer {
   public static void main(String[] args) {
-    SpringApplication.run(CashControlApplication.class, args);
+    SpringApplication.run(CashControlServer.class, args);
   }
 }
