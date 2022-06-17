3f98f707f46e7c20fe52c2b043a3538339e31008
Hayel <ivanforwork@yandex.ru>
2022-06-18 00:57:51 +0300

    Hooks added.

---
+++ frontend/cash-control-client/src/Hooks/useStore.js
@@ -0,0 +1,8 @@
+import React  from 'react';
+import stores from '../Stores';
+
+const storesContext = React.createContext(stores);
+
+export const useStores = () => React.useContext(storesContext);
+
+export const useStore = (store) => React.useContext(storesContext)[store];
