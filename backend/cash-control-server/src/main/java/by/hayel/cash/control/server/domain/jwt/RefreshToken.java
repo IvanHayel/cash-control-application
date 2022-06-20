736fb5353f4e5c0d6bcb7e35f4b4b0acba7bb7f7
Hayel <ivanforwork@yandex.ru>
2022-06-20 04:15:09 +0300

    Domains updated.

--- backend/cash-control-server/src/main/java/by/hayel/cash/control/server/domain/jwt/RefreshToken.java
+++ backend/cash-control-server/src/main/java/by/hayel/cash/control/server/domain/jwt/RefreshToken.java
@@ -6,7 +6,13 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import java.time.Instant;
 
 @Entity(name = "refresh_token")
--- backend/cash-control-server/src/main/java/by/hayel/cash/control/server/domain/user/Role.java
+++ backend/cash-control-server/src/main/java/by/hayel/cash/control/server/domain/user/Role.java
@@ -1,8 +1,20 @@
 package by.hayel.cash.control.server.domain.user;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.lang.NonNull;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
 @Table(
