3086fba3a894cfaaa2fc878a9a89bf4e1b102d0f
Hayel <ivanforwork@yandex.ru>
2022-06-20 04:18:30 +0300

    Wallet service added.

---
+++ backend/cash-control-server/src/main/java/by/hayel/cash/control/server/service/WalletService.java
@@ -0,0 +1,18 @@
+package by.hayel.cash.control.server.service;
+
+import by.hayel.cash.control.server.domain.wallet.Currency;
+import by.hayel.cash.control.server.domain.wallet.Wallet;
+
+import java.util.Collection;
+
+public interface WalletService {
+  Collection<Wallet> getWalletsByOwnerId(Long ownerId);
+
+  Wallet getWalletById(Long id);
+
+  void deleteById(Long walletId);
+
+  void save(Wallet wallet);
+
+  Currency parseCurrency(String currency);
+}
---
+++ backend/cash-control-server/src/main/java/by/hayel/cash/control/server/service/impl/ServerWalletService.java
@@ -0,0 +1,53 @@
+package by.hayel.cash.control.server.service.impl;
+
+import by.hayel.cash.control.server.domain.user.User;
+import by.hayel.cash.control.server.domain.wallet.Currency;
+import by.hayel.cash.control.server.domain.wallet.Wallet;
+import by.hayel.cash.control.server.exception.WalletNotFoundException;
+import by.hayel.cash.control.server.repository.WalletRepository;
+import by.hayel.cash.control.server.service.UserService;
+import by.hayel.cash.control.server.service.WalletService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class ServerWalletService implements WalletService {
+    WalletRepository repository;
+    UserService userService;
+
+    @Override
+    public Collection<Wallet> getWalletsByOwnerId(Long ownerId) {
+        User user = userService.getUserById(ownerId);
+        return repository.findAllByOwner(user);
+    }
+
+    @Override
+    public Wallet getWalletById(Long id) {
+        return repository.findById(id).orElseThrow(() -> new WalletNotFoundException(id));
+    }
+
+    @Override
+    public void deleteById(Long walletId) {
+        repository.deleteById(walletId);
+    }
+
+    @Override
+    public void save(Wallet wallet) {
+        repository.save(wallet);
+    }
+
+    @Override
+    public Currency parseCurrency(String currency) {
+        return switch (currency.toUpperCase()) {
+            case "USD" -> Currency.USD;
+            case "EUR" -> Currency.EUR;
+            default -> Currency.UNKNOWN;
+        };
+    }
+}
