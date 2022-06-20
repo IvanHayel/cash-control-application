3e516b1efe43f0eaa1fcd0076d0693a5a54b74d1
Hayel <ivanforwork@yandex.ru>
2022-06-20 07:43:09 +0300

    About route updated.

--- frontend/cash-control-client/src/Modules/About/About.jsx
+++ frontend/cash-control-client/src/Modules/About/About.jsx
@@ -1,5 +1,18 @@
-import React from 'react';
+import './Styles/About.scss';
+import React                            from 'react';
+import {Container, Divider, Typography} from '@mui/material';
 
 export const About = () => (
-    <h1>About</h1>
+    <Container className="about-container">
+      <Typography className="about-title" variant="h3">
+        ABOUT
+      </Typography>
+      <Divider variant="middle" />
+      <Typography className="about-text" variant="h5">
+        <strong>Cash Control</strong> is a web application that allows you to
+        track your cash flow. It is a simple way to keep track of your
+        cash flow and to make sure you are not spending more than you have.
+      </Typography>
+      <Divider variant="middle" />
+    </Container>
 );
